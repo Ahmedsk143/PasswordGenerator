82decf4693b1e3da0a93a01d8fb18319dafded71
Ahmed <ahmedsayedkhalil143@gmail.com>
2021-04-16 05:48:41 +0200

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  password = '';
+  length = 0;
+  check1 = false;
+  check2 = false;
+  check3 = false;
+
+  onInput(input: string) {
+    const parsInput = parseInt(input);
+    if (!isNaN(parsInput)) {
+      this.length = parsInput;
+    }
+    console.log(input);
+  }
+  onCheck1() {
+    this.check1 = !this.check1;
+  }
+  onCheck2() {
+    this.check2 = !this.check2;
+  }
+  onCheck3() {
+    this.check3 = !this.check3;
+  }
+  onClick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+';
+
+    let validChars = '';
+    if (this.check1) {
+      validChars += letters;
+    }
+    if (this.check2) {
+      validChars += numbers;
+    }
+    if (this.check3) {
+      validChars += symbols;
+    }
+    let generatedPass = '';
+    for (let i = 0; i < this.length; i++) {
+      // const rand = Math.random();
+      // console.log(rand);
+      // const floor = Math.floor(rand)
+      // console.log(floor);
+      // const mult = rand*20;
+      // console.log(mult);
+      // const multfloor = Math.floor(mult);
+      // console.log(multfloor);
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPass += validChars[index];
+    }
+    this.password = generatedPass;
+  }
+}
